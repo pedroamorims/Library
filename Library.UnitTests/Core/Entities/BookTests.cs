cd73f3c39399bf21d4e2c1c47649ed87712e99ea
pedroamorims <pedrohenrique.as18@gmail.com>
2024-05-28 18:55:24 -0300

    Implementing UnitTests

---
+++ Library.UnitTests/Core/Entities/BookTests.cs
@@ -0,0 +1,27 @@
+﻿using Library.Core.Entities;
+
+namespace Library.UnitTests.Core.Entities
+{
+    public class BookTests
+    {
+        [Fact]
+        public void TestIfBookActivateWorks()
+        {
+            var book = new Book("Nome de Teste","Author de Teste","ISBN de Teste",1234);
+
+            book.Activate();
+
+            Assert.True(book.Active);
+        }
+
+        [Fact]
+        public void TestIfBookDeactivateWorks()
+        {
+            var book = new Book("Nome de Teste", "Author de Teste", "ISBN de Teste", 1234);
+
+            book.Deactivate();
+
+            Assert.False(book.Active);
+        }
+    }
+}
---
+++ Library.UnitTests/Core/Entities/UserTests.cs
@@ -0,0 +1,27 @@
+﻿using Library.Core.Entities;
+
+namespace Library.UnitTests.Core.Entities
+{
+    public class UserTests
+    {
+        [Fact]
+        public void TestIfUserActivateWorks()
+        {
+            var user = new User("Nome de Teste","Email de Teste","Senha de Teste","admin");
+
+            user.Activate();
+
+            Assert.True(user.Active);
+        }
+
+        [Fact]
+        public void TestIfUserDeactivateWorks()
+        {
+            var user = new User("Nome de Teste", "Email de Teste", "Senha de Teste", "admin");
+
+            user.Deactivate();
+
+            Assert.False(user.Active);
+        }
+    }
+}
